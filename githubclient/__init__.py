eaec88eca96f8f7299807132b4ad9c66dbd2e4df
Stenio L. de Souza <steniols@gmail.com>
2021-04-23 13:45:18 -0300

    Update SQLAlchemy version and some imports

--- githubclient/__init__.py
+++ githubclient/__init__.py
@@ -1,16 +1,20 @@
-import os 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from githubclient.config import Config
-from flask_dance.contrib.github import make_github_blueprint, github
+import os
+
 from dotenv import load_dotenv
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_dance.contrib.github import make_github_blueprint
+from flask_sqlalchemy import SQLAlchemy
+
+from githubclient.config import Config
+
 load_dotenv()
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -19,17 +23,19 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
 
+    from githubclient.errors.handlers import errors
     from githubclient.main.routes import main
     from githubclient.tags.routes import tags
-    from githubclient.errors.handlers import errors
+
     app.register_blueprint(main)
     app.register_blueprint(tags)
     app.register_blueprint(errors)
 
-    github_blueprint = make_github_blueprint(client_id=os.getenv('CLIENT_ID'),
-                                           client_secret=os.getenv('CLIENT_SECRET'))
+    github_blueprint = make_github_blueprint(
+        client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET")
+    )
 
-    app.register_blueprint(github_blueprint, url_prefix='/github_login')
+    app.register_blueprint(github_blueprint, url_prefix="/github_login")
 
     with app.app_context():
         db.create_all()
